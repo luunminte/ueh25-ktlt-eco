9934dad413606db8df8440f69923bc1e23bd7a8d
luunminte <luunminh2510@gmail.com>
2025-08-26 16:10:41 +0700

    Delete buoi02_250816/.ipynb_checkpoints directory

--- buoi02_250816/.ipynb_checkpoints/DoSangRadian-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import math
-
-deg = float(input("Nhap goc (do): "))
-rad = deg * math.pi / 180.0
-print(f"{deg:g} do = {rad:g} rad")
--- buoi02_250816/.ipynb_checkpoints/ReverseWord-checkpoint.py
+++
@@ -1,7 +0,0 @@
-# Nhập số dòng
-n = int(input())
-
-for _ in range(n):
-    s = input().split()       # tách từ theo khoảng trắng
-    s.reverse()               # đảo ngược danh sách từ
-    print(" ".join(s))        # ghép lại thành chuỗi
